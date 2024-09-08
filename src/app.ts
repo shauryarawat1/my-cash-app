8440d4a4abad6eb7e002801a8b399febf8c70105
Shaurya Rawat <realshauryarawat@hotmail.com>
2024-09-08 14:42:04 -0400

    Updated app file with auth routes

--- src/app.ts
+++ src/app.ts
@@ -1,8 +1,14 @@
 import express, {Express, Request, Response, NextFunction} from 'express';
+import connectDB from './db';
+import router from './routes/auth';
 const app: Express = express();
+
+connectDB();
 
 // Middleware
 app.use(express.json());
+
+app.use('/api/auth', router);
 
 // Error handling
 app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
