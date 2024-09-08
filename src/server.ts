b8df1729bbf4f3e470b42dcb37c0622c42fc514a
Shaurya Rawat <realshauryarawat@hotmail.com>
2024-09-08 13:50:51 -0400

    Set up a basic server by calling app

--- src/server.ts
+++ src/server.ts
@@ -1,18 +1,7 @@
-import express, {Express, Request, Response, NextFunction} from 'express';
-const app: Express = express();
+import app from './app';
 
-// Middleware
-app.use(express.json());
+const port = process.env.PORT || 3000;
 
-// Error handling
-app.use((err: Error, req: Request, res: Response, next: NextFunction) => {
-    console.error(err.stack);
-    res.status(500).send("Something broke!");
+app.listen(port, () => {
+    console.log("Server running on port ${port}");
 });
-
-// Basic route
-app.get('/', (req: Request, res: Response) => {
-    res.send("Welcome to MyCash App!");
-});
-
-export default app;
